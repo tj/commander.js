569b1373ba5f045cd213110e11224264eb808e18
Tj Holowaychuk <tj@vision-media.ca>
2011-08-14 13:06:38 -0700

    more tests

--- test/test.options.bool.no.js
+++ test/test.options.bool.no.js
@@ -8,7 +8,7 @@ var program = require('../')
 program
   .version('0.0.1')
   .option('-p, --pepper', 'add pepper')
-  .option('-c, --no-cheese', 'remove cheese');
+  .option('-c|--no-cheese', 'remove cheese');
 
 program.parse(['node', 'test', '--no-cheese']);
 should.equal(undefined, program.pepper);
