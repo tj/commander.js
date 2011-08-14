98f4ca0b05f0cdd87a93ad65aa44fd453c2ffdc3
Tj Holowaychuk <tj@vision-media.ca>
2011-08-14 11:57:44 -0700

    When --no-* is used default to true

---
+++ test/test.options.bool.js
@@ -0,0 +1,16 @@
+
+/**
+ * Module dependencies.
+ */
+
+var program = require('../')
+  , should = require('should');
+
+program
+  .version('0.0.1')
+  .option('-p, --pepper', 'add pepper')
+  .option('-c, --no-cheese', 'remove cheese');
+
+program.parse(['node', 'test', '--pepper']);
+program.pepper.should.be.true;
+program.cheese.should.be.true;
