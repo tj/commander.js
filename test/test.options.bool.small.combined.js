fdfd9d25feb66109fc8c4ed2b97969e3131fb6cb
Tj Holowaychuk <tj@vision-media.ca>
2011-08-14 12:16:06 -0700

    expand small flags

---
+++ test/test.options.bool.small.combined.js
@@ -0,0 +1,15 @@
+/**
+ * Module dependencies.
+ */
+
+var program = require('../')
+  , should = require('should');
+
+program
+  .version('0.0.1')
+  .option('-p, --pepper', 'add pepper')
+  .option('-c, --no-cheese', 'remove cheese');
+
+program.parse(['node', 'test', '-pc']);
+program.pepper.should.be.true;
+program.cheese.should.be.false;
