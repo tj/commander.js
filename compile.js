b7cc8697e5c09bf7d7e63eb0957974dd75f2b5e4
Tj Holowaychuk <tj@vision-media.ca>
2011-10-22 14:49:29 -0700

    misc

--- compile.js
+++ compile.js
@@ -10,8 +10,14 @@ var path = process.argv[2];
 var obj = JSON.parse(fs.readFileSync(path, 'utf8'));
 var tmpl = fs.readFileSync('index.jade', 'utf8');
 
-jade.render(tmpl, { comments: obj }, function(err, html){
+jade.render(tmpl, { ignore: ignore, comments: obj }, function(err, html){
   if (err) throw err;
   process.stdout.write(html);
 });
 
+function ignore(comment) {
+  return comment.ignore
+    || !comment.ctx
+    || 'version' == comment.ctx.name
+    || ~comment.description.full.indexOf('Module dependencies');
+}
