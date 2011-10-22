b3b93dc2337ac967d96864bdd6fa9d336ab069f4
Tj Holowaychuk <tj@vision-media.ca>
2011-10-22 14:55:07 -0700

    titles

--- compile.js
+++ compile.js
@@ -10,10 +10,25 @@ var path = process.argv[2];
 var obj = JSON.parse(fs.readFileSync(path, 'utf8'));
 var tmpl = fs.readFileSync('index.jade', 'utf8');
 
-jade.render(tmpl, { id: id, ignore: ignore, comments: obj }, function(err, html){
+var locals = {
+    id: id
+  , ignore: ignore
+  , comments: obj
+  , title: title
+};
+
+jade.render(tmpl, locals, function(err, html){
   if (err) throw err;
   process.stdout.write(html);
 });
+
+function title(comment) {
+  if (comment.ctx.string.indexOf('prototype')) {
+    return comment.ctx.string.replace('.prototype.', '#');
+  } else {
+    return comment.ctx.string;
+  }
+}
 
 function id(comment) {
   return comment.ctx.string
