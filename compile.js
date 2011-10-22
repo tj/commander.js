8df59efc86a5f60fec003cd78883b2af1e55269c
Tj Holowaychuk <tj@vision-media.ca>
2011-10-22 14:53:21 -0700

    links

--- compile.js
+++ compile.js
@@ -10,10 +10,15 @@ var path = process.argv[2];
 var obj = JSON.parse(fs.readFileSync(path, 'utf8'));
 var tmpl = fs.readFileSync('index.jade', 'utf8');
 
-jade.render(tmpl, { ignore: ignore, comments: obj }, function(err, html){
+jade.render(tmpl, { id: id, ignore: ignore, comments: obj }, function(err, html){
   if (err) throw err;
   process.stdout.write(html);
 });
+
+function id(comment) {
+  return comment.ctx.string
+    .replace('()', '');
+}
 
 function ignore(comment) {
   return comment.ignore
