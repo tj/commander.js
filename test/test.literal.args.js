ad17f171a2132fd1392405c364ea3f82464ea575
Tj Holowaychuk <tj@vision-media.ca>
2011-11-15 08:04:50 -0800

    Added support for --. Closes #24

---
+++ test/test.literal.args.js
@@ -0,0 +1,15 @@
+/**
+ * Module dependencies.
+ */
+
+var program = require('../')
+  , should = require('should');
+
+program
+  .version('0.0.1')
+  .option('-f, --foo', 'add some foo')
+  .option('-b, --bar', 'add some bar');
+
+program.parse(['node', 'test', '--foo', '--', '--bar', 'baz']);
+program.foo.should.be.true;
+program.args.should.eql(['--bar', 'baz']);
