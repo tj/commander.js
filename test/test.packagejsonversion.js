095d38d75cdd6b678bef1d35a3fb6296b90648c3
Sébastien De Bollivier <webinadiv@gmail.com>
2012-05-04 20:18:58 +0400

    Issue #48 : Take -version from package.json

--- test/test.packagejsonversion.js
+++ test/test.packagejsonversion.js
@@ -6,14 +6,13 @@ var program = require('../')
   , should = require('should');
 
 program
-  .version(program.packageVersion('1.3'))
   .option('-f, --foo', 'add some foo')
   .option('-b, --bar', 'add some bar');
 
-should.equal(program.packageVersion(), program.version());
+should.equal(require('../package.json').version, program.version());
 
 program
-  .version(program.packageVersion('1.3', '../test.json'))
+  .version('1.3')
   .option('-f, --foo', 'add some foo')
   .option('-b, --bar', 'add some bar');
 should.equal('1.3', program.version());
