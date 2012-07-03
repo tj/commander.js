5f5bd43da76db81b98afb66873ded51ba5faf1c0
Sébastien De Bollivier <webinadiv@gmail.com>
2012-07-03 11:00:16 +0400

    Essai

--- test/test.packagejsonversion.js
+++ test/test.packagejsonversion.js
@@ -2,17 +2,24 @@
  * Module dependencies.
  */
 
-var program = require('../')
-  , should = require('should');
+// var program = require('../')
+//   , should = require('should');
 
-program
-  .option('-f, --foo', 'add some foo')
-  .option('-b, --bar', 'add some bar');
+// program
+//   .option('-f, --foo', 'add some foo')
+//   .option('-b, --bar', 'add some bar');
 
-should.equal(require('../package.json').version, program.version());
+// should.equal(require('../package.json').version, program.version());
+// var program2 = require('../');
+// program2
+//   .version('1.3')
+//   .option('-f, --foo', 'add some foo')
+//   .option('-b, --bar', 'add some bar');
+// should.equal('1.3', program2.version());
 
-program
+var program3 = require('../');
+program3
   .version('1.3')
   .option('-f, --foo', 'add some foo')
   .option('-b, --bar', 'add some bar');
-should.equal('1.3', program.version());
+// should.equal('1.3', program3.version());
