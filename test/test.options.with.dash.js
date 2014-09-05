877258fe2b5cf7c88530b5275045d77b324e00d5
Henry Tseng <henryiis@gmail.com>
2014-09-05 12:45:24 -0400

    Added test case for dashes in options

---
+++ test/test.options.with.dash.js
@@ -0,0 +1,19 @@
+/**
+ * Module dependencies.
+ */
+
+var program = require('../')
+  , should = require('should');
+
+function parseRange(str) {
+  return str.split('..').map(Number);
+}
+
+program
+  .version('0.0.1')
+  .option('-d, --donuts [num]', -10)
+  .option('-a, --eater <person>', 'Sally Sue')
+
+program.parse('node test -d -8,2,2.32 -a Richard'.split(' '));
+program.donuts.should.equal('-8,2,2.32');
+program.eater.should.equal('Richard');
