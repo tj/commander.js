609f6a63d54b344a0af69625947ecdb265b2f461
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2015-06-18 08:13:34 +0200

    added sigint test

--- test/test.command.killsubcommand.js
+++ test/test.command.killsubcommand.js
@@ -2,12 +2,9 @@ var exec = require('child_process').exec
   , path = require('path')
   , should = require('should');
 
-
-
 var bin = path.join(__dirname, './fixtures/pm')
 
 var procSIGHUP = exec(bin + ' service', function (error, stdout, stderr) {
-
   should.equal(0, procSIGHUP.exitCode);
 });
 
@@ -15,7 +12,16 @@ var procSIGTERM = exec(bin + ' service', function (error, stdout, stderr) {
   should.equal(0, procSIGTERM.exitCode);
 });
 
+var procSIGINT = exec(bin + ' service', function (error, stdout, stderr) {
+  should.equal(0, procSIGINT.exitCode);
+});
+
 setTimeout(function(){
   process.kill(procSIGHUP.pid, 'SIGHUP');
+},100);
+setTimeout(function(){
   process.kill(procSIGTERM.pid, 'SIGTERM');
-},200);
+},100);
+setTimeout(function(){
+  process.kill(procSIGINT.pid, 'SIGINT');
+},100);
