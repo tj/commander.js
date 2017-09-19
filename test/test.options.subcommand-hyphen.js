aed923dd64d6417fe893b71f1a636e33a8ef4c44
jumpei-miyata <jumpei-miyata@cybozu.co.jp>
2017-09-19 12:37:23 +0900

    fix indent

--- test/test.options.subcommand-hyphen.js
+++ test/test.options.subcommand-hyphen.js
@@ -3,16 +3,16 @@
  */
 
 var program = require('../')
-    , should = require('should');
+  , should = require('should');
 
 program
-    .command('subcommand')
-    .description('description')
-    .option('-a, --alpha <a>', 'hyphen')
-    .option('-b, --bravo <b>', 'hyphen')
-    .option('-c, --charlie <c>', 'hyphen')
-    .action(function (options) {
-    });
+  .command('subcommand')
+  .description('description')
+  .option('-a, --alpha <a>', 'hyphen')
+  .option('-b, --bravo <b>', 'hyphen')
+  .option('-c, --charlie <c>', 'hyphen')
+  .action(function (options) {
+  });
 
 program.parse('node test subcommand -a - --bravo - --charlie=-'.split(' '));
 
