2365feb28bb4874744c859edaf42115187d21886
Dan Allen <dan.j.allen@gmail.com>
2018-01-04 23:40:14 -0700

    resolves #739 add parent command as prefix of subcommand

--- index.js
+++ index.js
@@ -1031,9 +1031,12 @@ Command.prototype.helpInformation = function() {
   if (this._alias) {
     cmdName = cmdName + '|' + this._alias;
   }
+  var ancestor = this
+  var prefix = []
+  while ((ancestor = ancestor.parent)) prefix.push(ancestor.name() + ' ')
   var usage = [
     ''
-    ,'  Usage: ' + cmdName + ' ' + this.usage()
+    ,'  Usage: ' + prefix.join('') + cmdName + ' ' + this.usage()
     , ''
   ];
 
