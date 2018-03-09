7209147de82dac781aab85a1bb62e13abb9339b3
abetomo <abe@enzou.tokyo>
2018-03-09 19:49:35 +0900

    Fix to emit the action even without command

--- index.js
+++ index.js
@@ -660,7 +660,7 @@ Command.prototype.parseArgs = function(args, unknown) {
     if (unknown.length > 0) {
       this.unknownOption(unknown[0]);
     }
-    if (this._args.filter(a => a.required).length == 0) {
+    if (this._args.filter(a => a.required).length === 0) {
       this.emit('command:*');
     }
   }
