5a27ec4e58c38c7ef9479bad246e3d771129e404
John Gee <john@ruru.gen.nz>
2019-06-30 12:52:51 +1200

    Add support for default value for boolean flags

--- test/test.options.defaults.given.js
+++ test/test.options.defaults.given.js
@@ -8,15 +8,19 @@ var program = require('../')
 program
   .version('0.0.1')
   .option('-a, --anchovies', 'Add anchovies?')
-  .option('-o, --onions', 'Add onions?', true)
+  .option('-o, --onions', 'Add onions?')
+  .option('-O, --no-onions', 'No onions')
+  .option('-t, --tomatoes', 'Add tomatoes?', false)
+  .option('-T, --no-tomatoes', 'No tomatoes')
   .option('-v, --olives', 'Add olives? Sorry we only have black.', 'black')
   .option('-s, --no-sauce', 'Uh… okay')
   .option('-r, --crust <type>', 'What kind of crust would you like?', 'hand-tossed')
   .option('-c, --cheese [type]', 'optionally specify the type of cheese', 'mozzarella');
 
-program.parse(['node', 'test', '--anchovies', '--onions', '--olives', '--no-sauce', '--crust', 'thin', '--cheese', 'wensleydale']);
+program.parse(['node', 'test', '--anchovies', '--onions', '--tomatoes', '--olives', '--no-sauce', '--crust', 'thin', '--cheese', 'wensleydale']);
 program.should.have.property('anchovies', true);
 program.should.have.property('onions', true);
+program.should.have.property('tomatoes', true);
 program.should.have.property('olives', 'black');
 program.should.have.property('sauce', false);
 program.should.have.property('crust', 'thin');
--- test/test.options.defaults.js
+++ test/test.options.defaults.js
@@ -8,7 +8,10 @@ var program = require('../')
 program
   .version('0.0.1')
   .option('-a, --anchovies', 'Add anchovies?')
-  .option('-o, --onions', 'Add onions?', true)
+  .option('-o, --onions', 'Add onions?')
+  .option('-O, --no-onions', 'No onions')
+  .option('-t, --tomatoes', 'Add tomatoes?', false)
+  .option('-T, --no-tomatoes', 'No tomatoes')
   .option('-v, --olives', 'Add olives? Sorry we only have black.', 'black')
   .option('-s, --no-sauce', 'Uh… okay')
   .option('-r, --crust <type>', 'What kind of crust would you like?', 'hand-tossed')
@@ -20,6 +23,7 @@ program.parse(['node', 'test']);
 program.should.have.property('_name', 'test');
 program.should.not.have.property('anchovies');
 program.should.not.have.property('onions');
+program.should.have.property('tomatoes', false);
 program.should.not.have.property('olives');
 program.should.have.property('sauce', true);
 program.should.have.property('crust', 'hand-tossed');
