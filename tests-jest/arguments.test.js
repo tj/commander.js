1580ac8a7d7c7849d3e1b375b7d33fd60b3bb2d2
John Gee <john@ruru.gen.nz>
2019-07-28 15:40:19 +1200

    Prototype Jest as testing framework

---
+++ tests-jest/arguments.test.js
@@ -0,0 +1,34 @@
+/**
+ * Top-level command syntax.
+ */
+
+const commander = require('../');
+
+function makeProgram() {
+  const program = new commander.Command();
+  const programActionHandler = jest.fn();
+  program
+    .version('0.0.1')
+    .arguments('<cmd> [env]')
+    .action(programActionHandler)
+    .option('-C, --chdir <path>', 'change the working directory')
+    .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+    .option('-T, --no-tests', 'ignore test hook');
+  return { program, programActionHandler };
+}
+
+test('option without arguments', () => {
+  const { program, programActionHandler } = makeProgram();
+  program.parse(['node', 'test', '--config', 'conf']);
+  expect(program.config).toBe('conf');
+  // Historically allowed to call program without the required argument [sic]
+  expect(programActionHandler).toHaveBeenCalledTimes(0);
+});
+
+test('options mixed into arguments', () => {
+  const { program, programActionHandler } = makeProgram();
+  program.parse(['node', 'test', '--config', 'conf2', 'setup', '--setup_mode', 'mode3', 'env1']);
+  expect(program.config).toBe('conf2');
+  expect(programActionHandler).toHaveBeenCalledTimes(1);
+  expect(programActionHandler).toHaveBeenCalledWith('setup', 'env1', expect.anything());
+});
---
+++ tests-jest/command.action.test.js
@@ -0,0 +1,16 @@
+var program = require('../');
+
+test('option set on command', () => {
+  // (Somewhat odd test converted directly from original.)
+  var val = false;
+  program
+    .command('info [options]')
+    .option('-C, --no-color', 'turn off color output')
+    .action(function() {
+      val = this.color;
+    });
+
+  program.parse(['node', 'test', 'info']);
+
+  expect(program.commands[0].color).toBe(val);
+});
