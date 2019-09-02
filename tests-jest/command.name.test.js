af0225e1fb74200a69da02d562b2b80e538fba8a
John Gee <john@ruru.gen.nz>
2019-09-02 21:41:30 +1200

    Port some of command.name tests

---
+++ tests-jest/command.name.test.js
@@ -0,0 +1,21 @@
+const commander = require('../');
+
+test('when set program name and parse then name is as assigned', () => {
+  const program = new commander.Command();
+  program.name('custom');
+  program.parse(['node', 'test']);
+  expect(program.name()).toBe('custom');
+});
+
+test('when program name not set and parse then name is found from arguments', () => {
+  const program = new commander.Command();
+  program.parse(['node', 'test']);
+  expect(program.name()).toBe('test');
+});
+
+test('when add command then command is named', () => {
+  const program = new commander.Command();
+  program
+    .command('mycommand [options]');
+  expect(program.commands[0].name()).toBe('mycommand');
+});
--- tests-jest/test-ideas.md
+++ tests-jest/test-ideas.md
@@ -9,3 +9,5 @@ Noting testing ideas as working through existing tests to check whether covered 
 - how much is parsing of program same as parsing of command, how much needs testing both program and command?
 - add a test of global Command exposed on import
 - test `.on()` variations
+- test.command.name.js was testing exit if no subcommmand when using executable subcommands (good test), and that help command added (bad test?).
+- test.command.name.set.js was testing program.description
