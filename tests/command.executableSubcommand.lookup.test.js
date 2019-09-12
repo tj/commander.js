846c8519ebed66827209c94bb8bea719523bbdc9
John Gee <john@ruru.gen.nz>
2019-09-12 20:24:03 +1200

    Fix error detection on node 8

--- tests/command.executableSubcommand.lookup.test.js
+++ tests/command.executableSubcommand.lookup.test.js
@@ -62,7 +62,11 @@ test('when subcommand target executablefile has suffix .js then lookup succeeds'
 // This is not a behaviour we enforce but rather one that is expected.
 test('when subcommand file not executable then error', (done) => {
   childProcess.execFile(bin, ['search'], { }, function(error, stdout, stderr) {
-    expect(error.toString()).toMatch('not executable');
+    // In node 8 the EACCES gets thrown instead of emitted through the spawned process.
+    // In node 10 and 12 we get the commander error.
+    // commander: error: %s(1) not executable. try chmod or run with root
+    // node: Error: Command failed: ... Error: spawn EACCES
+    expect(error.toString()).toMatch(new RegExp('not executable|EACCES'));
     done();
   });
 });
