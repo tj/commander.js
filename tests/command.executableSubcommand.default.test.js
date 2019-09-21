c49f12b6c8daeb5872d17a5ee8b30ce112ebdaf6
John Gee <john@ruru.gen.nz>
2019-09-21 14:36:26 +1200

    Call default subcommand when there are unknown options on command line.
    Co-authored-by: zhuqiacheng <zhuqiacheng@meizu.com>

--- tests/command.executableSubcommand.default.test.js
+++ tests/command.executableSubcommand.default.test.js
@@ -10,9 +10,16 @@ test('when default subcommand and no command then call default', (done) => {
   });
 });
 
-test('when default subcommand and unrecognised argument then call default', (done) => {
-  childProcess.execFile(bin, ['unrecognised-argument'], { }, function(_error, stdout, stderr) {
-    expect(stdout).toBe('default\n');
+test('when default subcommand and unrecognised argument then call default with argument', (done) => {
+  childProcess.execFile(bin, ['an-argument'], { }, function(_error, stdout, stderr) {
+    expect(stdout).toBe("default\n[ 'an-argument' ]\n");
+    done();
+  });
+});
+
+test('when default subcommand and unrecognised option then call default with option', (done) => {
+  childProcess.execFile(bin, ['--an-option'], { }, function(_error, stdout, stderr) {
+    expect(stdout).toBe("default\n[ '--an-option' ]\n");
     done();
   });
 });
