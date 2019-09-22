0349d6a9261bd1a3d8935449ee7f1043d81e8c05
John Gee <john@ruru.gen.nz>
2019-09-22 20:50:21 +1200

    Hacking to pass more tests on WIndows

--- tests/command.executableSubcommand.default.test.js
+++ tests/command.executableSubcommand.default.test.js
@@ -4,21 +4,21 @@ const path = require('path');
 const bin = path.join(__dirname, './fixtures/pm');
 
 test('when default subcommand and no command then call default', (done) => {
-  childProcess.execFile(bin, [], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('default\n');
     done();
   });
 });
 
 test('when default subcommand and unrecognised argument then call default with argument', (done) => {
-  childProcess.execFile(bin, ['an-argument'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'an-argument'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe("default\n[ 'an-argument' ]\n");
     done();
   });
 });
 
 test('when default subcommand and unrecognised option then call default with option', (done) => {
-  childProcess.execFile(bin, ['--an-option'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, '--an-option'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe("default\n[ '--an-option' ]\n");
     done();
   });
--- tests/command.executableSubcommand.lookup.test.js
+++ tests/command.executableSubcommand.lookup.test.js
@@ -4,56 +4,56 @@ const path = require('path');
 const bin = path.join(__dirname, './fixtures/pm');
 
 test('when subcommand file missing then error', (done) => {
-  childProcess.execFile(bin, ['list'], function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'list'], function(_error, stdout, stderr) {
     expect(stderr).toBe('error: pm-list(1) does not exist, try --help\n');
     done();
   });
 });
 
 test('when alias subcommand file missing then error', (done) => {
-  childProcess.execFile(bin, ['lst'], function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'lst'], function(_error, stdout, stderr) {
     expect(stderr).toBe('error: pm-list(1) does not exist, try --help\n');
     done();
   });
 });
 
 test('when subcommand file has no suffix then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['install'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'install'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when alias subcommand file has no suffix then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['i'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'i'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when subcommand target executablefile has no suffix then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['specifyInstall'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'specifyInstall'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when subcommand file suffix .js then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['publish'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'publish'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('publish\n');
     done();
   });
 });
 
 test('when alias subcommand file suffix .js then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['p'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'p'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('publish\n');
     done();
   });
 });
 
 test('when subcommand target executablefile has suffix .js then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['specifyPublish'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'specifyPublish'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('publish\n');
     done();
   });
@@ -61,7 +61,7 @@ test('when subcommand target executablefile has suffix .js then lookup succeeds'
 
 // This is not a behaviour we enforce but rather one that is expected.
 test('when subcommand file not executable then error', (done) => {
-  childProcess.execFile(bin, ['search'], { }, function(error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'search'], { }, function(error, stdout, stderr) {
     // In node 8 the EACCES gets thrown instead of emitted through the spawned process.
     // In node 10 and 12 we get the commander error.
     // commander: error: %s(1) not executable. try chmod or run with root
@@ -73,7 +73,7 @@ test('when subcommand file not executable then error', (done) => {
 
 test('when subcommand file is symlink then lookup succeeds', (done) => {
   const binLink = path.join(__dirname, './fixtures/pmlink');
-  childProcess.execFile(binLink, ['install'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [binLink, 'install'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
@@ -81,7 +81,7 @@ test('when subcommand file is symlink then lookup succeeds', (done) => {
 
 test('when subcommand file is double symlink then lookup succeeds', (done) => {
   const binLink = path.join(__dirname, './fixtures/another-dir/pm');
-  childProcess.execFile(binLink, ['install'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [binLink, 'install'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
@@ -96,7 +96,7 @@ test('when subcommand suffix is .ts then lookup succeeds', (done) => {
 });
 
 test('when subsubcommand then lookup sub-sub-command', (done) => {
-  childProcess.execFile(bin, ['cache', 'clear'], { }, function(_error, stdout, stderr) {
+  childProcess.execFile('node', [bin, 'cache', 'clear'], { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('cache-clear\n');
     done();
   });
