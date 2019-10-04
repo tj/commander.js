efe685638cd691a340d5741e397817af4cacd0e0
John Gee <john@ruru.gen.nz>
2019-10-04 14:12:10 +1300

    Use node explicitly for compatibility with WIndows

--- tests/command.executableSubcommand.lookup.test.js
+++ tests/command.executableSubcommand.lookup.test.js
@@ -1,59 +1,61 @@
 const childProcess = require('child_process');
 const path = require('path');
 
-const bin = path.join(__dirname, './fixtures/pm');
+// Calling node explicitly so pm works without file suffix cross-platform.
+
+const pm = path.join(__dirname, './fixtures/pm');
 
 test('when subcommand file missing then error', (done) => {
-  childProcess.execFile(bin, ['list'], function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} list`, function(_error, stdout, stderr) {
     expect(stderr).toBe('error: pm-list(1) does not exist, try --help\n');
     done();
   });
 });
 
 test('when alias subcommand file missing then error', (done) => {
-  childProcess.execFile(bin, ['lst'], function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} lst`, function(_error, stdout, stderr) {
     expect(stderr).toBe('error: pm-list(1) does not exist, try --help\n');
     done();
   });
 });
 
 test('when subcommand file has no suffix then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['install'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} install`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when alias subcommand file has no suffix then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['i'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} i`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when subcommand target executablefile has no suffix then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['specifyInstall'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} specifyInstall`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when subcommand file suffix .js then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['publish'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} publish`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('publish\n');
     done();
   });
 });
 
 test('when alias subcommand file suffix .js then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['p'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} p`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('publish\n');
     done();
   });
 });
 
 test('when subcommand target executablefile has suffix .js then lookup succeeds', (done) => {
-  childProcess.execFile(bin, ['specifyPublish'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} specifyPublish`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('publish\n');
     done();
   });
@@ -61,7 +63,7 @@ test('when subcommand target executablefile has suffix .js then lookup succeeds'
 
 // This is not a behaviour we enforce but rather one that is expected.
 test('when subcommand file not executable then error', (done) => {
-  childProcess.execFile(bin, ['search'], { }, function(error, stdout, stderr) {
+  childProcess.exec(`node ${pm} search`, { }, function(error, stdout, stderr) {
     // In node 8 the EACCES gets thrown instead of emitted through the spawned process.
     // In node 10 and 12 we get the commander error.
     // commander: error: %s(1) not executable. try chmod or run with root
@@ -72,16 +74,16 @@ test('when subcommand file not executable then error', (done) => {
 });
 
 test('when subcommand file is symlink then lookup succeeds', (done) => {
-  const binLink = path.join(__dirname, './fixtures/pmlink');
-  childProcess.execFile(binLink, ['install'], { }, function(_error, stdout, stderr) {
+  const pmlink = path.join(__dirname, './fixtures/pmlink');
+  childProcess.exec(`node ${pmlink} install`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
 });
 
 test('when subcommand file is double symlink then lookup succeeds', (done) => {
-  const binLink = path.join(__dirname, './fixtures/another-dir/pm');
-  childProcess.execFile(binLink, ['install'], { }, function(_error, stdout, stderr) {
+  const pmlink = path.join(__dirname, './fixtures/another-dir/pm');
+  childProcess.exec(`node ${pmlink} install`, { }, function(_error, stdout, stderr) {
     expect(stdout).toBe('install\n');
     done();
   });
@@ -96,7 +98,7 @@ test('when subcommand suffix is .ts then lookup succeeds', (done) => {
 });
 
 test('when subsubcommand then lookup sub-sub-command', (done) => {
-  childProcess.execFile(bin, ['cache', 'clear'], { }, function(_error, stdout, stderr) {
+  childProcess.exec(`node ${pm} cache clear`, function(_error, stdout, stderr) {
     expect(stdout).toBe('cache-clear\n');
     done();
   });
