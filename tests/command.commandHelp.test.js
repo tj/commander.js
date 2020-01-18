6b7a7c6e6ee047af699000fa7c961ba071d46eaf
John Gee <john@ruru.gen.nz>
2020-01-18 22:57:34 +1300

    Remove addHelpCommand from tests and make match more narrow

--- tests/command.commandHelp.test.js
+++ tests/command.commandHelp.test.js
@@ -5,17 +5,15 @@ const commander = require('../');
 test('when program has command then appears in commandHelp', () => {
   const program = new commander.Command();
   program
-    .addHelpCommand(false)
     .command('bare');
   const commandHelp = program.commandHelp();
-  expect(commandHelp).toBe('Commands:\n  bare\n');
+  expect(commandHelp).toMatch(/Commands:\n +bare\n/);
 });
 
 test('when program has command with optional arg then appears in commandHelp', () => {
   const program = new commander.Command();
   program
-    .addHelpCommand(false)
     .command('bare [bare-arg]');
   const commandHelp = program.commandHelp();
-  expect(commandHelp).toEqual('Commands:\n  bare [bare-arg]\n');
+  expect(commandHelp).toMatch(/Commands:\n +bare \[bare-arg\]\n/);
 });
