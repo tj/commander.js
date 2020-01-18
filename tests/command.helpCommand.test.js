b4c055b9b36839b8dfb069b4d3f8dae14cef780c
John Gee <john@ruru.gen.nz>
2020-01-19 08:24:07 +1300

    Add tests that help command dispatched to correct command

--- tests/command.helpCommand.test.js
+++ tests/command.helpCommand.test.js
@@ -1,6 +1,6 @@
 const commander = require('../');
 
-describe('help command in displayed help', () => {
+describe('help command listed in helpInformation', () => {
   test('when program has no subcommands then no automatic help command', () => {
     const program = new commander.Command();
     const helpInformation = program.helpInformation();
@@ -34,5 +34,67 @@ describe('help command in displayed help', () => {
     program.addHelpCommand('help command', 'help description');
     const helpInformation = program.helpInformation();
     expect(helpInformation).toMatch(/help command +help description/);
+  });
+});
+
+describe('help command processed on correct command', () => {
+  // Use internal knowledge to suppress output to keep test output clean.
+  let consoleErrorSpy;
+  let writeSpy;
+
+  beforeAll(() => {
+    consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => { });
+    writeSpy = jest.spyOn(process.stdout, 'write').mockImplementation(() => { });
+  });
+
+  afterEach(() => {
+    consoleErrorSpy.mockClear();
+    writeSpy.mockClear();
+  });
+
+  afterAll(() => {
+    consoleErrorSpy.mockRestore();
+    writeSpy.mockRestore();
+  });
+
+  test('when "program help" then program', () => {
+    const program = new commander.Command();
+    program.exitOverride();
+    program.command('sub1');
+    program.exitOverride(() => { throw new Error('program'); });
+    expect(() => {
+      program.parse('node test.js help'.split(' '));
+    }).toThrow('program');
+  });
+
+  test('when "program help sub1" then sub1', () => {
+    const program = new commander.Command();
+    program.exitOverride();
+    const sub1 = program.command('sub1');
+    sub1.exitOverride(() => { throw new Error('sub1'); });
+    expect(() => {
+      program.parse('node test.js help sub1'.split(' '));
+    }).toThrow('sub1');
+  });
+
+  test('when "program sub1 help sub2" then sub2', () => {
+    const program = new commander.Command();
+    program.exitOverride();
+    const sub1 = program.command('sub1');
+    const sub2 = sub1.command('sub2');
+    sub2.exitOverride(() => { throw new Error('sub2'); });
+    expect(() => {
+      program.parse('node test.js sub1 help sub2'.split(' '));
+    }).toThrow('sub2');
+  });
+
+  test('when default command and "program help" then program', () => {
+    const program = new commander.Command();
+    program.exitOverride();
+    program.command('sub1', { isDefault: true });
+    program.exitOverride(() => { throw new Error('program'); });
+    expect(() => {
+      program.parse('node test.js help'.split(' '));
+    }).toThrow('program');
   });
 });
