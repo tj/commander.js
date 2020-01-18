c4557328f28b9bdbedbe4c8a7f07d4b1983e375a
John Gee <john@ruru.gen.nz>
2020-01-19 08:30:39 +1300

    Add simple nested subcommand test

---
+++ tests/command.nested.test.js
@@ -0,0 +1,12 @@
+const commander = require('../');
+
+test('when call nested subcommand then runs', () => {
+  const program = new commander.Command();
+  const leafAction = jest.fn();
+  program
+    .command('sub1')
+    .command('sub2')
+    .action(leafAction);
+  program.parse('node test.js sub1 sub2'.split(' '));
+  expect(leafAction).toHaveBeenCalled();
+});
