4043f2b0c991847390b17950d028d9772f212418
John Gee <john@ruru.gen.nz>
2020-01-22 21:02:49 +1300

    Move common command configuration options in README, and add isDefault example program

---
+++ examples/defaultCommand.js
@@ -0,0 +1,36 @@
+// const commander = require('commander'); // (normal include)
+const commander = require('../'); // include commander in git clone of commander repo
+const program = new commander.Command();
+
+// Example program using the command configuration option isDefault to specify the default command.
+//
+// $ node defaultCommand.js build
+// build
+// $ node defaultCommand.js serve -p 8080
+// server on port 8080
+// $ node defaultCommand.js -p 443
+// server on port 443
+
+program
+  .command('build')
+  .description('build web site for deployment')
+  .action(() => {
+    console.log('build');
+  });
+
+program
+  .command('deploy')
+  .description('deploy web site to production')
+  .action(() => {
+    console.log('deploy');
+  });
+
+program
+  .command('serve', { isDefault: true })
+  .description('launch web server')
+  .option('-p,--port <port_number>', 'web port')
+  .action((opts) => {
+    console.log(`server on port ${opts.port}`);
+  });
+
+program.parse(process.argv);
