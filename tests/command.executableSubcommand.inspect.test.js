36e707dd304ee29c9071fe3874d51410f14cf32e
John Gee <john@ruru.gen.nz>
2020-02-04 00:34:26 +1300

    Rework exec tests to use promises instead of jest callback

--- tests/command.executableSubcommand.inspect.test.js
+++ tests/command.executableSubcommand.inspect.test.js
@@ -1,69 +1,56 @@
 const childProcess = require('child_process');
 const path = require('path');
+const util = require('util');
+
+const execFileAsync = util.promisify(childProcess.execFile);
 
 // Test the special handling for --inspect to increment fixed debug port numbers.
 // If we reuse port we can get conflicts because port not released fast enough.
 
 const inspectCommand = path.join(__dirname, './fixtures', 'inspect.js');
 
-test('when execArgv empty then spawn execArgs empty', (done) => {
-  childProcess.execFile('node', [inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe('[]\n');
-    done();
-  });
+test('when execArgv empty then spawn execArgs empty', async() => {
+  const { stdout } = await execFileAsync('node', [inspectCommand, 'sub']);
+  expect(stdout).toBe('[]\n');
 });
 
-test('when execArgv --harmony then spawn execArgs --harmony', (done) => {
-  childProcess.execFile('node', ['--harmony', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--harmony' ]\n");
-    done();
-  });
+test('when execArgv --harmony then spawn execArgs --harmony', async() => {
+  const { stdout } = await execFileAsync('node', ['--harmony', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--harmony' ]\n");
 });
 
 // --inspect defaults to 127.0.0.1:9229, port should be incremented
-test('when execArgv --inspect then spawn execArgs using port 9230', (done) => {
-  childProcess.execFile('node', ['--inspect', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--inspect=127.0.0.1:9230' ]\n");
-    done();
-  });
+test('when execArgv --inspect then spawn execArgs using port 9230', async() => {
+  const { stdout } = await execFileAsync('node', ['--inspect', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--inspect=127.0.0.1:9230' ]\n");
 });
 
 // custom port
-test('when execArgv --inspect=9240 then spawn execArgs using port 9241', (done) => {
-  childProcess.execFile('node', ['--inspect=9240', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--inspect=127.0.0.1:9241' ]\n");
-    done();
-  });
+test('when execArgv --inspect=9240 then spawn execArgs using port 9241', async() => {
+  const { stdout } = await execFileAsync('node', ['--inspect=9240', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--inspect=127.0.0.1:9241' ]\n");
 });
 
 // zero is special, random port
-test('when execArgv --inspect=0 then spawn execArgs --inspect=0', (done) => {
-  childProcess.execFile('node', ['--inspect=0', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--inspect=0' ]\n");
-    done();
-  });
+test('when execArgv --inspect=0 then spawn execArgs --inspect=0', async() => {
+  const { stdout } = await execFileAsync('node', ['--inspect=0', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--inspect=0' ]\n");
 });
 
 // ip address
-test('when execArgv --inspect=127.0.0.1:9250 then spawn execArgs --inspect=127.0.0.1:9251', (done) => {
-  childProcess.execFile('node', ['--inspect=127.0.0.1:9250', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--inspect=127.0.0.1:9251' ]\n");
-    done();
-  });
+test('when execArgv --inspect=127.0.0.1:9250 then spawn execArgs --inspect=127.0.0.1:9251', async() => {
+  const { stdout } = await execFileAsync('node', ['--inspect=127.0.0.1:9250', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--inspect=127.0.0.1:9251' ]\n");
 });
 
 // localhost
-test('when execArgv --inspect=localhost:9260 then spawn execArgs --inspect=localhost:9261', (done) => {
-  childProcess.execFile('node', ['--inspect=localhost:9260', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--inspect=localhost:9261' ]\n");
-    done();
-  });
+test('when execArgv --inspect=localhost:9260 then spawn execArgs --inspect=localhost:9261', async() => {
+  const { stdout } = await execFileAsync('node', ['--inspect=localhost:9260', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--inspect=localhost:9261' ]\n");
 });
 
 // --inspect-port, just test most likely format
-test('when execArgv --inspect-port=9270 then spawn execArgs --inspect-port=127.0.0.1:9271', (done) => {
-  childProcess.execFile('node', ['--inspect-port=9270', inspectCommand, 'sub'], function(_error, stdout, stderr) {
-    expect(stdout).toBe("[ '--inspect-port=127.0.0.1:9271' ]\n");
-    done();
-  });
+test('when execArgv --inspect-port=9270 then spawn execArgs --inspect-port=127.0.0.1:9271', async() => {
+  const { stdout } = await execFileAsync('node', ['--inspect-port=9270', inspectCommand, 'sub']);
+  expect(stdout).toBe("[ '--inspect-port=127.0.0.1:9271' ]\n");
 });
