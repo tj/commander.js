4bc95d04e17deda179f3a25af3b1f0ffd8873817
John Gee <john@ruru.gen.nz>
2020-09-01 23:03:27 +1200

    Fix write mocks for help-as-error output

--- tests/command.executableSubcommand.test.js
+++ tests/command.executableSubcommand.test.js
@@ -5,7 +5,7 @@ const commander = require('../');
 // This is the default behaviour when no default command and no action handlers
 test('when no command specified and executable then display help', () => {
   // Optional. Suppress normal output to keep test output clean.
-  const writeSpy = jest.spyOn(process.stdout, 'write').mockImplementation(() => { });
+  const writeSpy = jest.spyOn(process.stderr, 'write').mockImplementation(() => { });
   const program = new commander.Command();
   program
     .exitOverride((err) => { throw err; })
