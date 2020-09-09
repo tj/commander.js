8401ee64a291ef53665b7dba100fcd0c70193cdf
John Gee <john@ruru.gen.nz>
2020-09-09 21:26:54 +1200

    Add chaining test for addHelpText

--- tests/command.chain.test.js
+++ tests/command.chain.test.js
@@ -123,4 +123,10 @@ describe('Command methods that should return this for chaining', () => {
     const result = program.helpOption(false);
     expect(result).toBe(program);
   });
+
+  test('when call .addHelpText() then returns this', () => {
+    const program = new Command();
+    const result = program.addHelpText('before', 'example');
+    expect(result).toBe(program);
+  });
 });
