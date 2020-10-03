a946cda0f0ed13b4557d21f6aa1a21235b8ed261
John Gee <john@ruru.gen.nz>
2020-10-03 18:37:48 +1300

    Add commandUsage tests

--- tests/help.test.js
+++ tests/help.test.js
@@ -224,3 +224,48 @@ describe('largestArgTermLength', () => {
     expect(helper.largestArgTermLength(program, helper)).toEqual('longest'.length);
   });
 });
+
+describe('commandUsage', () => {
+  test('when single program then "Usage: program [options]"', () => {
+    const program = new commander.Command();
+    program.name('program');
+    const helper = new commander.Help();
+    expect(helper.commandUsage(program)).toEqual('Usage: program [options]');
+  });
+
+  test('when multi program then "Usage: program [options] [command]"', () => {
+    const program = new commander.Command();
+    program.name('program');
+    program.command('sub');
+    const helper = new commander.Help();
+    expect(helper.commandUsage(program)).toEqual('Usage: program [options] [command]');
+  });
+
+  test('when program has alias then usage includes alias', () => {
+    const program = new commander.Command();
+    program
+      .name('program')
+      .alias('alias');
+    const helper = new commander.Help();
+    expect(helper.commandUsage(program)).toEqual('Usage: program|alias [options]');
+  });
+
+  test('when help for subcommand then usage includes hierarchy', () => {
+    const program = new commander.Command();
+    program
+      .name('program');
+    const sub = program.command('sub')
+      .name('sub');
+    const helper = new commander.Help();
+    expect(helper.commandUsage(sub)).toEqual('Usage: program sub [options]');
+  });
+
+  test('when program has argument then usage includes argument', () => {
+    const program = new commander.Command();
+    program
+      .name('program')
+      .arguments('<file>');
+    const helper = new commander.Help();
+    expect(helper.commandUsage(program)).toEqual('Usage: program [options] <file>');
+  });
+});
