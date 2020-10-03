b96c5283fa15021c9f83d2e155a0f50f862f46d4
John Gee <john@ruru.gen.nz>
2020-10-03 20:27:53 +1300

    Add sort tests

--- tests/help.test.js
+++ tests/help.test.js
@@ -3,6 +3,72 @@ const commander = require('../');
 // These are tests of the Help class, not of the Command help.
 // There is some overlap with the higher level Command tests (which predate Help).
 
+describe.skip('columns', () => {
+  // To Do
+});
+
+describe('sortCommands', () => {
+  test('when unsorted then commands in order added', () => {
+    const program = new commander.Command();
+    program
+      .command('ccc', 'desc')
+      .command('aaa', 'desc')
+      .command('bbb', 'desc');
+    const helper = program.createHelp();
+    const visibleCommandNames = helper.visibleCommands(program).map(cmd => cmd.name());
+    expect(visibleCommandNames).toEqual(['ccc', 'aaa', 'bbb', 'help']);
+  });
+
+  test('when sortCommands:true then commands sorted', () => {
+    const program = new commander.Command();
+    program
+      .configureHelp({ sortCommands: true })
+      .command('ccc', 'desc')
+      .command('aaa', 'desc')
+      .command('bbb', 'desc');
+    const helper = program.createHelp();
+    const visibleCommandNames = helper.visibleCommands(program).map(cmd => cmd.name());
+    expect(visibleCommandNames).toEqual(['aaa', 'bbb', 'ccc', 'help']);
+  });
+});
+
+describe('sortOptions', () => {
+  test('when unsorted then options in order added', () => {
+    const program = new commander.Command();
+    program
+      .option('--zzz', 'desc')
+      .option('--aaa', 'desc')
+      .option('--bbb', 'desc');
+    const helper = program.createHelp();
+    const visibleOptionNames = helper.visibleOptions(program).map(option => option.name());
+    expect(visibleOptionNames).toEqual(['zzz', 'aaa', 'bbb', 'help']);
+  });
+
+  test('when sortOptions:true then options sorted', () => {
+    const program = new commander.Command();
+    program
+      .configureHelp({ sortOptions: true })
+      .option('--zzz', 'desc')
+      .option('--aaa', 'desc')
+      .option('--bbb', 'desc');
+    const helper = program.createHelp();
+    const visibleCommandNames = helper.visibleOptions(program).map(cmd => cmd.name());
+    expect(visibleCommandNames).toEqual(['aaa', 'bbb', 'help', 'zzz']);
+  });
+
+  test('when sortOptions:true then options sorted on name not flags', () => {
+    const program = new commander.Command();
+    program
+      .configureHelp({ sortOptions: true })
+      .option('-m,--zzz', 'desc')
+      .option('-n,--aaa', 'desc')
+      .option('-o,--bbb', 'desc');
+    const helper = program.createHelp();
+    const visibleCommandNames = helper.visibleOptions(program).map(cmd => cmd.name());
+    expect(visibleCommandNames).toEqual(['aaa', 'bbb', 'help', 'zzz']);
+  });
+});
+
 describe('visibleCommands', () => {
   test('when no subcommands then empty array', () => {
     const program = new commander.Command();
