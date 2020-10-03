c26f94320063b8d66aa5f71d41a1f7ebc679f96b
John Gee <john@ruru.gen.nz>
2020-10-04 11:42:40 +1300

    Add tests for createHelp and configureHelp

---
+++ tests/command.createHelp.test.js
@@ -0,0 +1,18 @@
+const commander = require('../');
+
+test('when override createCommand then affects help', () => {
+  class MyHelp extends commander.Help {
+    formatHelp(cmd, helper) {
+      return 'custom';
+    }
+  }
+
+  class MyCommand extends commander.Command {
+    createHelp() {
+      return Object.assign(new MyHelp(), this.configureHelp());
+    };
+  }
+
+  const program = new MyCommand();
+  expect(program.helpInformation()).toEqual('custom');
+});
