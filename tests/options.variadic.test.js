02f5b44f85dd0d703055d53103272c99fafe0b98
John Gee <john@ruru.gen.nz>
2020-10-31 21:58:28 +1300

    Use configureOutput instead of jest.spyon for some tests

--- tests/options.variadic.test.js
+++ tests/options.variadic.test.js
@@ -2,19 +2,15 @@ const commander = require('../');
 
 describe('variadic option with required value', () => {
   test('when variadic with value missing then error', () => {
-    // Optional. Use internal knowledge to suppress output to keep test output clean.
-    const writeErrorSpy = jest.spyOn(process.stderr, 'write').mockImplementation(() => { });
-
     const program = new commander.Command();
     program
       .exitOverride()
+      .configureOutput({ writeError: jest.fn() })
       .option('-r,--required <value...>');
 
     expect(() => {
       program.parse(['--required'], { from: 'user' });
     }).toThrow();
-
-    writeErrorSpy.mockRestore();
   });
 
   test('when variadic with one value then set in array', () => {
