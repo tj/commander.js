8ee8635b8998f22dd30b2eb578c82ee8bb3bb379
John Gee <john@ruru.gen.nz>
2021-01-17 09:23:40 +1300

    Add esm wrapper and conditional exports

---
+++ wrapper.mjs
@@ -0,0 +1,8 @@
+import commander from "./index.js"
+
+export const program = commander.program;
+export const Option = commander.Option;
+export const Command = commander.Command;
+export const CommanderError = commander.CommanderError;
+export const InvalidOptionArgumentError = commander.InvalidOptionArgumentError;
+export const Help = commander.Help;
