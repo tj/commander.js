ff338de64d287062177ce3627747f2a6cb79cc74
John Gee <john@ruru.gen.nz>
2021-01-17 13:25:50 +1300

    createCommand is an implict export, make explicit

--- wrapper.mjs
+++ wrapper.mjs
@@ -6,3 +6,4 @@ export const Command = commander.Command;
 export const CommanderError = commander.CommanderError;
 export const InvalidOptionArgumentError = commander.InvalidOptionArgumentError;
 export const Help = commander.Help;
+export const createCommand = commander.createCommand;
