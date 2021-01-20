81db72f64f08560a1f230c365b9659bb398b0627
John Gee <john@ruru.gen.nz>
2021-01-20 17:23:54 +1300

    fix typo

--- esm.mjs
+++ esm.mjs
@@ -1,4 +1,4 @@
 import commander from './index.js';
 
-// wrapper to provide names exports for ESM.
+// wrapper to provide named exports for ESM.
 export const { program, Option, Command, CommanderError, InvalidOptionArgumentError, Help, createCommand } = commander;
