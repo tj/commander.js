2e9573b46fc3b147cb02ce2f2778c853fb215f17
John Gee <john@ruru.gen.nz>
2021-01-30 17:56:41 +1300

    Lint, and simplify code

--- tests/fixtures-extensions/pm.js
+++ tests/fixtures-extensions/pm.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-var program = require('../../');
+const program = require('../../');
 
 program
-  .version('0.0.1')
   .command('try-ts', 'test file extension lookup')
   .command('try-cjs', 'test file extension lookup')
   .command('try-mjs', 'test file extension lookup')
