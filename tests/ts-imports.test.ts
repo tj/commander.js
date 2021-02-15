c119028f9fd66fbb1d850a8a66237fcb8a2d6f4a
John Gee <john@ruru.gen.nz>
2021-02-15 20:40:50 +1300

    Add runtime test that TypeScript imports are working (#1473)
    * Add runtime test that TypeScript imports are working
    
    * Add test of default export, have not deleted it yet!

---
+++ tests/ts-imports.test.ts
@@ -0,0 +1,44 @@
+import { program, Command, Option, CommanderError, InvalidOptionArgumentError, Help, createCommand } from '../';
+
+import * as commander from '../';
+
+// Do some simple checks that expected imports are available at runtime.
+// Similar tests to esm-imports-test.js
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+function checkClass(obj: object, name: string) {
+  expect(typeof obj).toEqual('object');
+  expect(obj.constructor.name).toEqual(name);
+}
+
+test('legacy default export of global Command', () => {
+  checkClass(commander, 'Command');
+});
+
+test('program', () => {
+  checkClass(program, 'Command');
+});
+
+test('createCommand', () => {
+  checkClass(createCommand(), 'Command');
+});
+
+test('Command', () => {
+  checkClass(new Command('name'), 'Command');
+});
+
+test('Option', () => {
+  checkClass(new Option('-e, --example', 'description'), 'Option');
+});
+
+test('CommanderError', () => {
+  checkClass(new CommanderError(1, 'code', 'failed'), 'CommanderError');
+});
+
+test('InvalidOptionArgumentError', () => {
+  checkClass(new InvalidOptionArgumentError('failed'), 'InvalidOptionArgumentError');
+});
+
+test('Help', () => {
+  checkClass(new Help(), 'Help');
+});
