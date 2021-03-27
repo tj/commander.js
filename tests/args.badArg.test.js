da374365d3ebfab76d92232148b75671ddcb7743
John Gee <john@ruru.gen.nz>
2021-03-27 17:00:30 +1300

    Fix test naming

--- tests/args.badArg.test.js
+++ tests/args.badArg.test.js
@@ -1,6 +1,6 @@
 const commander = require('../');
 
-test('should throw on bad argument', () => {
+test('when unexpected argument format then throw', () => {
   const program = new commander.Command();
   expect(() => program.addArgument(new commander.Argument('bad-format'))).toThrow();
   expect(() => program.argument('bad-format')).toThrow();
