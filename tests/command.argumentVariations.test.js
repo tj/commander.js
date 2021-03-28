0e505be606dcfba6fe03d4d9908c2ba8030adf95
John Gee <john@ruru.gen.nz>
2021-03-28 18:35:33 +1300

    Add low-level tests for multiple arg variations

--- tests/command.argumentVariations.test.js
+++ tests/command.argumentVariations.test.js
@@ -3,7 +3,7 @@ const commander = require('../');
 // Do some low-level checks that the multiple ways of specifying command arguments produce same internal result,
 // and not exhaustively testing all methods elsewhere.
 
-test.each(getTestCases('<explicit-required>'))('when add "<arg>" via %s then argument required', (methodName, cmd) => {
+test.each(getSingleArgCases('<explicit-required>'))('when add "<arg>" using %s then argument required', (methodName, cmd) => {
   const argument = cmd._args[0];
   const expectedShape = {
     _name: 'explicit-required',
@@ -14,7 +14,7 @@ test.each(getTestCases('<explicit-required>'))('when add "<arg>" via %s then arg
   expect(argument).toEqual(expectedShape);
 });
 
-test.each(getTestCases('implicit-required'))('when add "arg" via %s then argument required', (methodName, cmd) => {
+test.each(getSingleArgCases('implicit-required'))('when add "arg" using %s then argument required', (methodName, cmd) => {
   const argument = cmd._args[0];
   const expectedShape = {
     _name: 'implicit-required',
@@ -25,7 +25,7 @@ test.each(getTestCases('implicit-required'))('when add "arg" via %s then argumen
   expect(argument).toEqual(expectedShape);
 });
 
-test.each(getTestCases('[optional]'))('when add "[arg]" via %s then argument optional', (methodName, cmd) => {
+test.each(getSingleArgCases('[optional]'))('when add "[arg]" using %s then argument optional', (methodName, cmd) => {
   const argument = cmd._args[0];
   const expectedShape = {
     _name: 'optional',
@@ -36,7 +36,7 @@ test.each(getTestCases('[optional]'))('when add "[arg]" via %s then argument opt
   expect(argument).toEqual(expectedShape);
 });
 
-test.each(getTestCases('<explicit-required...>'))('when add "<arg...>" via %s then argument required and variadic', (methodName, cmd) => {
+test.each(getSingleArgCases('<explicit-required...>'))('when add "<arg...>" using %s then argument required and variadic', (methodName, cmd) => {
   const argument = cmd._args[0];
   const expectedShape = {
     _name: 'explicit-required',
@@ -47,7 +47,7 @@ test.each(getTestCases('<explicit-required...>'))('when add "<arg...>" via %s th
   expect(argument).toEqual(expectedShape);
 });
 
-test.each(getTestCases('implicit-required...'))('when add "arg..." via %s then argument required and variadic', (methodName, cmd) => {
+test.each(getSingleArgCases('implicit-required...'))('when add "arg..." using %s then argument required and variadic', (methodName, cmd) => {
   const argument = cmd._args[0];
   const expectedShape = {
     _name: 'implicit-required',
@@ -58,7 +58,7 @@ test.each(getTestCases('implicit-required...'))('when add "arg..." via %s then a
   expect(argument).toEqual(expectedShape);
 });
 
-test.each(getTestCases('[optional...]'))('when add "[arg...]" via %s then argument optional and variadic', (methodName, cmd) => {
+test.each(getSingleArgCases('[optional...]'))('when add "[arg...]" using %s then argument optional and variadic', (methodName, cmd) => {
   const argument = cmd._args[0];
   const expectedShape = {
     _name: 'optional',
@@ -69,7 +69,7 @@ test.each(getTestCases('[optional...]'))('when add "[arg...]" via %s then argume
   expect(argument).toEqual(expectedShape);
 });
 
-function getTestCases(arg) {
+function getSingleArgCases(arg) {
   return [
     ['.arguments', new commander.Command().arguments(arg)],
     ['.argument', new commander.Command().argument(arg)],
@@ -77,3 +77,28 @@ function getTestCases(arg) {
     ['.command', new commander.Command().command(`command ${arg}`)]
   ];
 }
+
+test.each(getMultipleArgCases('<first>', '[second]'))('when add two arguments using %s then two arguments', (methodName, cmd) => {
+  expect(cmd._args[0].name()).toEqual('first');
+  expect(cmd._args[1].name()).toEqual('second');
+});
+
+function getMultipleArgCases(arg1, arg2) {
+  return [
+    ['.arguments', new commander.Command().arguments(`${arg1} ${arg2}`)],
+    ['.argument', new commander.Command().argument(arg1).argument(arg2)],
+    ['.addArgument', new commander.Command('add-argument').addArgument(new commander.Argument(arg1)).addArgument(new commander.Argument(arg2))],
+    ['.command', new commander.Command().command(`command ${arg1} ${arg2}`)]
+  ];
+}
+
+test('when add arguments using multiple methods then all added', () => {
+  // This is not a key use case, but explicitly test that additive behaviour.
+  const program = new commander.Command();
+  const cmd = program.command('sub <arg1> <arg2>');
+  cmd.arguments('<arg3> <arg4>');
+  cmd.argument('<arg5>');
+  cmd.addArgument(new commander.Argument('arg6'));
+  const argNames = cmd._args.map(arg => arg.name());
+  expect(argNames).toEqual(['arg1', 'arg2', 'arg3', 'arg4', 'arg5', 'arg6']);
+});
