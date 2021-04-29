24080412bdd544f3a004b5276a07f60fe1806a33
John Gee <john@ruru.gen.nz>
2021-04-29 16:52:42 +1200

    Default only makes sense for optional argument

--- tests/help.argumentDescription.test.js
+++ tests/help.argumentDescription.test.js
@@ -4,26 +4,26 @@ const commander = require('../');
 
 describe('argumentDescription', () => {
   test('when argument has no description then empty string', () => {
-    const argument = new commander.Argument('<n>');
+    const argument = new commander.Argument('[n]');
     const helper = new commander.Help();
     expect(helper.argumentDescription(argument)).toEqual('');
   });
 
   test('when argument has description then return description', () => {
     const description = 'description';
-    const argument = new commander.Argument('<a>', description);
+    const argument = new commander.Argument('[n]', description);
     const helper = new commander.Help();
     expect(helper.argumentDescription(argument)).toEqual(description);
   });
 
   test('when argument has default value then return description and default value', () => {
-    const argument = new commander.Argument('<n>', 'description').default('default');
+    const argument = new commander.Argument('[n]', 'description').default('default');
     const helper = new commander.Help();
     expect(helper.argumentDescription(argument)).toEqual('description (default: "default")');
   });
 
   test('when argument has default value description then return description and custom default description', () => {
-    const argument = new commander.Argument('<n>', 'description').default('default value', 'custom');
+    const argument = new commander.Argument('[n]', 'description').default('default value', 'custom');
     const helper = new commander.Help();
     expect(helper.argumentDescription(argument)).toEqual('description (default: custom)');
   });
