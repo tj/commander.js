e398237a1f8eb804c082519cd42f87ad866b9706
John Gee <john@ruru.gen.nz>
2021-04-29 17:27:14 +1200

    Add argument custom processing to README

--- examples/argument.js
+++ examples/argument.js
@@ -9,11 +9,11 @@ const program = new Command();
 program
   .version('0.1.0')
   .argument('<username>', 'user to login')
-  .argument('[password]', 'password for user, if required')
+  .argument('[password]', 'password for user, if required', 'no password given')
   .description('example program for argument')
   .action((username, password) => {
     console.log('username:', username);
-    console.log('password:', password || 'no password given');
+    console.log('password:', password);
   });
 
 program.parse();
---
+++ examples/arguments-custom-processing.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+// This is used as an example in the README for:
+//    Custom argument processing
+//    You may specify a function to do custom processing of argument values.
+
+// const commander = require('commander'); // (normal include)
+const commander = require('../'); // include commander in git clone of commander repo
+const program = new commander.Command();
+
+function myParseInt(value, dummyPrevious) {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  // if (isNaN(parsedValue)) {
+  //   throw new commander.InvalidOptionArgumentError('Not a number.');
+  // }
+  return parsedValue;
+}
+
+program
+  .argument('<first>', 'integer argument', myParseInt)
+  .argument('[second]', 'integer argument', myParseInt, 1000)
+  .action((first, second) => {
+    console.log(`${first} + ${second} = ${first + second}`);
+  })
+;
+
+program.parse();
+
+// Try the following:
+//    node arguments-custom-processing --help
+//    node arguments-custom-processing 2
+//    node arguments-custom-processing 12 56
