947dbc36b46962c0dd0b9605ab91550d7d54d4e2
John Gee <john@ruru.gen.nz>
2021-07-11 19:03:01 +1200

    Add chain test

--- tests/argument.chain.test.js
+++ tests/argument.chain.test.js
@@ -18,4 +18,16 @@ describe('Argument methods that should return this for chaining', () => {
     const result = argument.choices(['a']);
     expect(result).toBe(argument);
   });
+
+  test('when call .argRequired() then returns this', () => {
+    const argument = new Argument('<value>');
+    const result = argument.argRequired();
+    expect(result).toBe(argument);
+  });
+
+  test('when call .argOptional() then returns this', () => {
+    const argument = new Argument('<value>');
+    const result = argument.argOptional();
+    expect(result).toBe(argument);
+  });
 });
