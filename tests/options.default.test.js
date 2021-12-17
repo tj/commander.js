d15519daf31066b293c960bfa925d6f3e43f77ec
John Gee <john@ruru.gen.nz>
2021-12-18 10:20:25 +1300

    Add tests that default does get overwritten

--- tests/options.default.test.js
+++ tests/options.default.test.js
@@ -117,3 +117,77 @@ describe('Option with default and option not specified in parse', () => {
     expect(program.opts().colour).toBe('RGB');
   });
 });
+
+// Fairly obvious this needs to happen, but was broken for optional in past!
+describe('default overwritten by specified option', () => {
+  test('when boolean option with boolean default then value is true', () => {
+    const program = new Command();
+    program.option('-d, --debug', 'description', false);
+    program.parse(['-d'], { from: 'user' });
+    expect(program.opts().debug).toBe(true);
+  });
+
+  test('when boolean option with number zero default then value is true', () => {
+    const program = new Command();
+    program.option('-d, --debug', 'description', 0);
+    program.parse(['-d'], { from: 'user' });
+    expect(program.opts().debug).toBe(true);
+  });
+
+  test('when boolean option with string default then value is true', () => {
+    const program = new Command();
+    program.option('-d, --debug', 'description', 'default');
+    program.parse(['-d'], { from: 'user' });
+    expect(program.opts().debug).toBe(true);
+  });
+
+  test('when required option-argument and default number then value is from args', () => {
+    const program = new Command();
+    program.option('-p, --port <port-number>', 'description', 80);
+    program.parse(['-p', '1234'], { from: 'user' });
+    expect(program.opts().port).toBe('1234');
+  });
+
+  test('when required option-argument and default string then value is from args', () => {
+    const program = new Command();
+    program.option('-p, --port <port-number>', 'description', 'foo');
+    program.parse(['-p', '1234'], { from: 'user' });
+    expect(program.opts().port).toBe('1234');
+  });
+
+  test('when optional option-argument and default number and option-argument specified then value is from args', () => {
+    const program = new Command();
+    program.option('-p, --port [port-number]', 'description', 80);
+    program.parse(['-p', '1234'], { from: 'user' });
+    expect(program.opts().port).toBe('1234');
+  });
+
+  test('when optional option-argument and default string and option-argument specified then value is from args', () => {
+    const program = new Command();
+    program.option('-p, --port [port-number]', 'description', 'foo');
+    program.parse(['-p', '1234'], { from: 'user' });
+    expect(program.opts().port).toBe('1234');
+  });
+
+  test('when optional option-argument and default number and option-argument not specified then value is true', () => {
+    const program = new Command();
+    program.option('-p, --port [port-number]', 'description', 80);
+    program.parse(['-p'], { from: 'user' });
+    expect(program.opts().port).toBe(true);
+  });
+
+  test('when optional option-argument and default string and option-argument not specified then value is true', () => {
+    const program = new Command();
+    program.option('-p, --port [port-number]', 'description', 'foo');
+    program.parse(['-p'], { from: 'user' });
+    expect(program.opts().port).toBe(true);
+  });
+
+  test('when negated and default string then value is false', () => {
+    // Bit tricky thinking about what default means for a negated option, but treat as with other options.
+    const program = new Command();
+    program.option('--no-colour', 'description', 'RGB');
+    program.parse(['--no-colour'], { from: 'user' });
+    expect(program.opts().colour).toBe(false);
+  });
+});
