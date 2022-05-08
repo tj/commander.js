d71470140804b1a558a5a45506f2dbac247e336c
John Gee <john@ruru.gen.nz>
2022-05-08 14:21:34 +1200

    Add first cut at implies and some tests

---
+++ tests/options.implies.test.js
@@ -0,0 +1,64 @@
+const { Command, Option } = require('../');
+
+describe('check priotities', () => {
+
+  test('when source undefined and implied undefined then implied is undefined', () => {
+    const program = new Command();
+    program
+      .addOption(new Option('--foo').implies({ bar: 'implied' }))
+      .option('--bar');
+    program.parse([], { from: 'user' });
+    expect(program.opts()).toEqual({});
+  });
+
+  test('when source default and implied undefined then implied is undefined', () => {
+    const program = new Command();
+    program
+      .addOption(new Option('--foo').implies({ bar: 'implied' }).default('default'))
+      .option('--bar');
+    program.parse([], { from: 'user' });
+    expect(program.opts()).toEqual({ foo: 'default' });
+  });
+
+  test('when source from env and implied undefined then implied is implied', () => {
+    const program = new Command();
+    const envName = 'COMMANDER_TEST_DELETE_ME';
+    process.env[envName] = 'env';
+    program
+      .addOption(new Option('--foo').implies({ bar: 'implied' }).env(envName))
+      .option('--bar');
+    program.parse([], { from: 'user' });
+    expect(program.opts()).toEqual({ foo: true, bar: 'implied' });
+    delete process.env[envName];
+  });
+
+  test('when source from cli and implied undefined then implied is implied', () => {
+    const program = new Command();
+    program
+      .addOption(new Option('--foo').implies({ bar: 'implied' }))
+      .option('--bar');
+    program.parse(['--foo'], { from: 'user' });
+    expect(program.opts()).toEqual({ foo: true, bar: 'implied' });
+  });
+
+  test('when source cli and implied default then implied is implied', () => {
+    const program = new Command();
+    program
+      .addOption(new Option('--foo').implies({ bar: 'implied' }))
+      .option('--bar', '', 'default');
+    program.parse(['--foo'], { from: 'user' });
+    expect(program.opts()).toEqual({ foo: true, bar: 'implied' });
+  });
+
+  test('when source cli and env default then implied is env', () => {
+    const program = new Command();
+    const envName = 'COMMANDER_TEST_DELETE_ME';
+    process.env[envName] = 'env';
+    program
+      .addOption(new Option('--foo').implies({ bar: 'implied' }))
+      .addOption(new Option('--bar <value>').env(envName));
+    program.parse(['--foo'], { from: 'user' });
+    expect(program.opts()).toEqual({ foo: true, bar: 'env' });
+    delete process.env[envName];
+  });
+});
