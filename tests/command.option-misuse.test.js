c778ab9dfdf9f00a7f25d4213af08a8592fbcecc
John Gee <john@ruru.gen.nz>
2022-05-21 22:09:31 +1200

    Add test for Option passed to wrong routines

---
+++ tests/command.option-misuse.test.js
@@ -0,0 +1,22 @@
+const { Command, Option } = require('../');
+
+// It is a reasonable and easy mistake to pass Option to .option(). Detect this
+// and offer advice.
+
+const expectedMessage = 'To add an Option object use addOption() instead of option() or requiredOption()';
+
+test('when pass Option to .option() then throw', () => {
+  const program = new Command();
+
+  expect(() => {
+    program.option(new Option('-d, debug'));
+  }).toThrow(expectedMessage);
+});
+
+test('when pass Option to .requiredOption() then throw', () => {
+  const program = new Command();
+
+  expect(() => {
+    program.requiredOption(new Option('-d, debug'));
+  }).toThrow(expectedMessage);
+});
