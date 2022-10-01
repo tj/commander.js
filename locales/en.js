1a08095abf05a3a4439da0f44a535cd1f63f4656
John Gee <john@ruru.gen.nz>
2022-10-01 13:53:14 +1300

    Translate suggestSimilar

--- locales/en.js
+++ locales/en.js
@@ -14,19 +14,23 @@ const translations = {
   "error: unknown option '{0}'": "error: unknown option '{0}'",
   // commander.unknownCommand
   "error: unknown command '{0}'": "error: unknown command '{0}'",
-  // commander.invalidArgument for option from cli
+  // commander.invalidArgument
+  // - option from cli
   "error: option '{0}' argument '{1}' is invalid.": "error: option '{0}' argument '{1}' is invalid.",
+  // - choices
   'Allowed choices are {0}.': 'Allowed choices are {0}.',
-  // commander.invalidArgument for option from env
+  // - option from env
   "error: option '{0}' value '{1}' from env '{2}' is invalid.": "error: option '{0}' value '{1}' from env '{2}' is invalid.",
-  // commander.invalidArgument for command-argument
+  // - command-argument
   "error: command-argument value '{0}' is invalid for argument '{1}": "error: command-argument value '{0}' is invalid for argument '{1}.",
   // ToDo
-  // - suggestSimilar for unknown option
-  // - suggestSimilar for and unknown command
   // - commander.excessArguments
   //   rework for program/subcommand, plural, multiple arguments ????
   // - order of error/detail for right to left languages ????
+
+  // suggest similar
+  '\n(Did you mean one of {0}?)': '\n(Did you mean one of {0}?)',
+  '\n(Did you mean {0}?)': '\n(Did you mean {0}?)',
 
   // help option, help command, version option
   // Needs refactor to work. Under user control anyway, so not essential.
