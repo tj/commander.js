5fe6ecb76dcf2b6d2496f45e5db5c3299bc83142
John Gee <john@ruru.gen.nz>
2022-10-04 19:42:49 +1300

    Placeholder example translation

---
+++ locales/eo-t-en.js
@@ -0,0 +1,36 @@
+// This is for testing and not intended for shipping.
+// esperanto translated from english (by Google Translate)
+
+const translations = {
+  "error: missing required argument '{0}'": "eraro: mankas postulata argumento '{0}'",
+  "error: option '{0}' argument missing": "eraro: opcio '{0}' argumento mankas",
+  "error: required option '{0}' not specified": "eraro: bezonata opcio '{0}' ne specifita",
+  'error: {0} cannot be used with {1}': 'eraro: {0} ne povas esti uzata kun {1}',
+  "error: unknown option '{0}'": "eraro: nekonata opcio '{0}'",
+  "error: unknown command '{0}'": "eraro: nekonata komando '{0}'",
+  "error: option '{0}' argument '{1}' is invalid.": "eraro: opcio '{0}' argumento '{1}' estas nevalida.",
+  'Allowed choices are {0}.': 'Allowed elektoj estas {0}.',
+  "error: option '{0}' value '{1}' from env '{2}' is invalid.": "eraro: opcio '{0}' valoro '{1}' de env '{2}' estas nevalida.",
+  "error: command-argument value '{0}' is invalid for argument '{1}": "eraro: komand-argumenta valoro '{0}' estas nevalida por argumento '{1}.",
+  "error: too many arguments for command '{0}'": "eraro: tro multaj argumentoj por komando '{0}'",
+  'error: too many arguments': 'eraro: tro multaj argumentoj',
+
+  '\n(Did you mean one of {0}?)': '\n(Ĉu vi volis diri unu el {0}?)',
+  '\n(Did you mean {0}?)': '\n(Ĉu vi celis {0}?)',
+
+  'output the version number': 'eligi la versinumeron',
+  'display help for command': 'montri helpon por komando',
+
+  'Usage:': 'Uzado:',
+  'Arguments:': 'Argumentoj:',
+  'Options:': 'Opcioj:',
+  'Commands:': 'Komandoj:',
+  '[options]': '[opcioj]',
+  '[command]': '[komando]',
+  'choices: {0}': 'elektoj: {0}',
+  'default: {0}': 'default: {0}',
+  'preset: {0}': 'malpostagordita: {0}',
+  'env: {0}': 'env: {0}'
+};
+
+exports.translations = translations;
