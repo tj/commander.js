08e3918fa6535081b978a9471b4b56db9fdd86c3
John Gee <john@ruru.gen.nz>
2022-11-02 22:01:48 +1300

    Add partial es translation

---
+++ locales/es-x-partial.js
@@ -0,0 +1,57 @@
+// Partial translation using strings from https://github.com/yargs/yargs/blob/main/locales/es.json
+
+const translations = {
+  // errors
+  // commander.missingArgument
+  "error: missing required argument '{0}'": "Error: falta argumento requerido '{0}'",
+  // commander.optionMissingArgument
+  // "error: option '{0}' argument missing": "error: option '{0}' argument missing",
+  // commander.missingMandatoryOptionValue
+  // "error: required option '{0}' not specified": "error: required option '{0}' not specified",
+  // commander.conflictingOption
+  // The environment variable is only displayed if caused conflict, and uses same format as in help.
+  // e.g. option '-r, --rat' (env: RAT) cannot be used with option '-c, --cat'
+  // "error: option '{0}'{1} cannot be used with option '{2}'{3}": "error: option '{0}'{1} cannot be used with option '{2}'{3}",
+  // commander.unknownOption
+  // "error: unknown option '{0}'": "error: unknown option '{0}'",
+  // commander.unknownCommand
+  // "error: unknown command '{0}'": "error: unknown command '{0}'",
+  // commander.invalidArgument
+  // - option from cli
+  // "error: option '{0}' argument '{1}' is invalid.": "error: option '{0}' argument '{1}' is invalid.",
+  // - choices, used for both option and argument
+  // 'Allowed choices are {0}.': 'Allowed choices are {0}.',
+  // - option from env
+  // "error: option '{0}' value '{1}' from env '{2}' is invalid.": "error: option '{0}' value '{1}' from env '{2}' is invalid.",
+  // - command-argument
+  // "error: command-argument value '{0}' is invalid for argument '{1}'.": "error: command-argument value '{0}' is invalid for argument '{1}'.",
+  // - commander.excessArguments
+  // "error: too many arguments for command '{0}'": "error: too many arguments for command '{0}'",
+  'error: too many arguments': 'Error: demasiados argumentos',
+
+  // suggest similar
+  // '(Did you mean one of {0}?)': '(Did you mean one of {0}?)',
+  // '(Did you mean {0}?)': '(Did you mean {0}?)',
+
+  // version option description
+  'output the version number': 'muestra número de versión',
+  // help option and help command description
+  'display help for command': 'muestra ayuda',
+
+  // Help
+  // titles
+  // 'Usage:': 'Usage:',
+  'Arguments:': 'Argumentos:',
+  'Options:': 'Opciones :',
+  'Commands:': 'Comandos:',
+  // usage
+  '[options]': '[opciones]',
+  '[command]': '[commando]',
+  // option details (after description)
+  'choices: {0}': 'selección: {0}',
+  'default: {0}': 'defecto: {0}'
+  // 'preset: {0}': 'preset: {0}',
+  // 'env: {0}': 'env: {0}'
+};
+
+exports.translations = translations;
