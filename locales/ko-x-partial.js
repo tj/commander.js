30daa3d01f0a05601854c310d0f8fdab9f2dc794
John Gee <john@ruru.gen.nz>
2022-11-03 21:18:13 +1300

    Add partial ko translation

---
+++ locales/ko-x-partial.js
@@ -0,0 +1,58 @@
+// Partial translation using strings from https://github.com/yargs/yargs/blob/main/locales/ko.json
+// Starting point for full translation, PR welcome!
+
+const translations = {
+  // errors
+  // commander.missingArgument
+  "error: missing required argument '{0}'": "오류: 필수 인수가 주어지지 않았습니다 '{0}'",
+  // commander.optionMissingArgument
+  // "error: option '{0}' argument missing": "error: option '{0}' argument missing",
+  // commander.missingMandatoryOptionValue
+  // "error: required option '{0}' not specified": "error: required option '{0}' not specified",
+  // commander.conflictingOption
+  // The environment variable is only displayed if caused conflict, and uses same format as in help.
+  // e.g. option '-r, --rat' (env: RAT) cannot be used with option '-c, --cat'
+  // "error: option '{0}'{1} cannot be used with option '{2}'{3}": "error: option '{0}'{1} cannot be used with option '{2}'{3}",
+  // commander.unknownOption
+  // "error: unknown option '{0}'": "error: unknown option '{0}'",
+  // commander.unknownCommand
+  // "error: unknown command '{0}'": "error: unknown command '{0}'",
+  // commander.invalidArgument
+  // - option from cli
+  // "error: option '{0}' argument '{1}' is invalid.": "error: option '{0}' argument '{1}' is invalid.",
+  // - choices, used for both option and argument
+  // 'Allowed choices are {0}.': 'Allowed choices are {0}.',
+  // - option from env
+  // "error: option '{0}' value '{1}' from env '{2}' is invalid.": "error: option '{0}' value '{1}' from env '{2}' is invalid.",
+  // - command-argument
+  // "error: command-argument value '{0}' is invalid for argument '{1}'.": "error: command-argument value '{0}' is invalid for argument '{1}'.",
+  // - commander.excessArguments
+  // "error: too many arguments for command '{0}'": "error: too many arguments for command '{0}'",
+  'error: too many arguments': '오류: 인수가 너무 많습니다',
+
+  // suggest similar
+  // '(Did you mean one of {0}?)': '(Did you mean one of {0}?)',
+  // '(Did you mean {0}?)': '(Did you mean {0}?)',
+
+  // version option description
+  'output the version number': '버전 표시',
+  // help option and help command description
+  'display help for command': '도움말 표시',
+
+  // Help
+  // titles
+  // 'Usage:': 'Usage:',
+  'Arguments:': '인수가:',
+  'Options:': '옵션:',
+  'Commands:': '명령:',
+  // usage
+  '[options]': '[옵션]',
+  '[command]': '[명령]',
+  // option details (after description)
+  'choices: {0}': '선택지: {0}',
+  'default: {0}': '기본값: {0}'
+  // 'preset: {0}': 'preset: {0}',
+  // 'env: {0}': 'env: {0}'
+};
+
+exports.translations = translations;
