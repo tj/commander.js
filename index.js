326df3cb3a0800e2251632c4ee5b8fa1a57822ca
Wee Bit <aweebit64@gmail.com>
2023-08-16 18:17:24 +0200

    Remove @ts-check directives
    The checkJs TSConfig option will be used instead.
    
    (Most of the directives were misplaced anyway.)

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { CommanderError, InvalidArgumentError } = require('./lib/error.js');
 const { Help } = require('./lib/help.js');
 const { Option } = require('./lib/option.js');
 
-// @ts-check
-
 /**
  * Expose the root command.
  */
