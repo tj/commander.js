8edcfd9826e076ef21f7e0b0da961b55816930e3
John Gee <john@ruru.gen.nz>
2023-09-11 08:57:52 +1200

    Work-around bug in Jest (#2011)
    Co-authored-by: Wee Bit <aweebit64@gmail.com>

--- tests/fixtures/inspect.js
+++ tests/fixtures/inspect.js
@@ -2,6 +2,8 @@
 
 const program = require('../../');
 
+process.env.FORCE_COLOR = 0; // work-around bug in Jest: https://github.com/jestjs/jest/issues/14391
+
 program
   .command('sub', 'install one or more packages')
   .parse(process.argv);
