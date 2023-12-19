6cf56ec2cffcf704385072f6461267175c783fae
John Gee <john@ruru.gen.nz>
2023-12-20 00:02:48 +1300

    Add TypeScript back into linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,20 @@ const globals = require("globals");
 const esLintjs = require("@eslint/js");
 const jest = require("eslint-plugin-jest");
 const prettier = require("eslint-config-prettier");
+const { FlatCompat } = require("@eslint/eslintrc"); // For reading original (non-flat) configs into flat format.
 
 // Jest is in process of updating for flat config support, so mixture of approaches.
 // https://github.com/jest-community/eslint-plugin-jest/issues/1408
 
+// Likewise with TypeScript, recommended approach is compatibility layer for now.
+// https://github.com/typescript-eslint/typescript-eslint/issues/7694
+
+const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
+
 module.exports = [
   esLintjs.configs.recommended,
-  prettier,
   {
-    files: ["**/*.js", "**/*.mjs", "*.cjs", "**/*.ts", "**/*.mts", "*.cts"],
+    files: ["**/*.{js,mjs,cjs}", "**/*.{ts,mts,cts}"],
     rules: {
       "no-unused-vars": "off", // lots in tests, minimise churn to start with
       'no-else-return': ['error', { allowElseIf: false }],
@@ -19,6 +24,19 @@ module.exports = [
       globals: {
         ...globals.node,
       }
+    }
+  },
+  // Hack, restricting TypeScript config to TypeScript files.
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+  ).map(c => ({
+    ...c,
+    files: ["**/*.{ts,mts,cts}"],
+  })),
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
     }
   },
   {
@@ -35,10 +53,5 @@ module.exports = [
       }
     }
   },
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-        sourceType: "commonjs"
-    }
-  }
+  prettier,
 ];
