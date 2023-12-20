ce6423bb4193f312ec577b79a4d1f6e7641ba28e
John Gee <john@ruru.gen.nz>
2023-12-20 17:47:46 +1300

    Apply prettier to lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-const globals = require("globals");
-const esLintjs = require("@eslint/js");
-const jest = require("eslint-plugin-jest");
-const prettier = require("eslint-config-prettier");
+const globals = require('globals');
+const esLintjs = require('@eslint/js');
+const jest = require('eslint-plugin-jest');
+const prettier = require('eslint-config-prettier');
 const jsdoc = require('eslint-plugin-jsdoc');
-const { FlatCompat } = require("@eslint/eslintrc"); // For reading original (non-flat) configs into flat format.
+const { FlatCompat } = require('@eslint/eslintrc'); // For reading original (non-flat) configs into flat format.
 
 // Jest is in process of updating for flat config support, so mixture of approaches.
 // https://github.com/jest-community/eslint-plugin-jest/issues/1408
@@ -17,44 +17,42 @@ module.exports = [
   esLintjs.configs.recommended,
   // jsdoc.configs['flat/recommended'],
   {
-    files: ["**/*.{js,mjs,cjs}", "**/*.{ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs}', '**/*.{ts,mts,cts}'],
     rules: {
-      "no-unused-vars": "off", // lots in tests, minimise churn to start with
+      'no-unused-vars': 'off', // lots in tests, minimise churn to start with
       'no-else-return': ['error', { allowElseIf: false }],
       // 'jsdoc/tag-lines': ['warn', "always", {"startLines":1}]
     },
     languageOptions: {
       globals: {
         ...globals.node,
-      }
-    }
+      },
+    },
   },
   // Hack, restricting TypeScript config to TypeScript files.
-  ...compat.extends(
-    'plugin:@typescript-eslint/recommended',
-  ).map(c => ({
+  ...compat.extends('plugin:@typescript-eslint/recommended').map((c) => ({
     ...c,
-    files: ["**/*.{ts,mts,cts}"],
+    files: ['**/*.{ts,mts,cts}'],
   })),
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      sourceType: "commonjs"
-    }
+      sourceType: 'commonjs',
+    },
   },
   {
-    files: ["**/*.test.js"],
+    files: ['**/*.test.js'],
     plugins: {
-      jest: jest
+      jest: jest,
     },
     rules: {
       ...jest.configs.recommended.rules,
     },
     languageOptions: {
       globals: {
-        ...globals.jest
-      }
-    }
+        ...globals.jest,
+      },
+    },
   },
   prettier,
 ];
