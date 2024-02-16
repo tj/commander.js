45164efc685f7ead15f4f54a33b04e5b7d9c7a22
John Gee <john@ruru.gen.nz>
2024-02-16 19:07:14 +1300

    Upgrade to typescript-eslint which supports eslint flat configs

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,14 @@ const globals = require('globals');
 const esLintjs = require('@eslint/js');
 const jest = require('eslint-plugin-jest');
 const prettier = require('eslint-config-prettier');
+const tseslint = require('typescript-eslint');
+
+
 // const jsdoc = require('eslint-plugin-jsdoc');
 const { FlatCompat } = require('@eslint/eslintrc'); // For reading original (non-flat) configs into flat format.
 
 // Jest is in process of updating for flat config support, so mixture of approaches.
 // https://github.com/jest-community/eslint-plugin-jest/issues/1408
-
-// Likewise with TypeScript, recommended approach is compatibility layer for now.
-// https://github.com/typescript-eslint/typescript-eslint/issues/7694
-
-const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
 
 module.exports = [
   esLintjs.configs.recommended,
@@ -30,8 +28,8 @@ module.exports = [
       },
     },
   },
-  // Hack, restricting TypeScript config to TypeScript files.
-  ...compat.extends('plugin:@typescript-eslint/recommended').map((c) => ({
+  // Restricting TypeScript configs to TypeScript files.
+  ...tseslint.configs.recommended.map((c) => ({
     ...c,
     files: ['**/*.{ts,mts,cts}'],
   })),
