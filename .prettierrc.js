b47d056e9b121fa0af633baeddc1d1ab8085af0b
John Gee <john@ruru.gen.nz>
2024-03-01 18:21:07 +1300

    Using comments in tsconfig*.json so be explicit that they are jsonc for Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,12 @@
 const config = {
-  "plugins": ["prettier-plugin-jsdoc"],
+  plugins: ['prettier-plugin-jsdoc'],
   singleQuote: true,
+  overrides: [
+    {
+      files: ['tsconfig*.json'],
+      options: { parser: 'jsonc' },
+    },
+  ],
 };
 
 module.exports = config;
