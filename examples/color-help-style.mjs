c4b85ba9d3b17f3f2e160ed85c9a465f41b929fc
John Gee <john@ruru.gen.nz>
2024-10-08 22:10:54 +1300

    Add more style hooks so can do usage strings without separate overrides

--- examples/color-help-style.mjs
+++ examples/color-help-style.mjs
@@ -8,35 +8,37 @@ import { Command } from 'commander';
 
 const program = new Command();
 
-program.usage(
-  `${styleText('green', '[options]')} ${styleText('yellow', '[command]')}`,
-);
 program.configureHelp({
   styleTitle: (str) => styleText('bold', str),
-  styleDescription: (str) => styleText('magenta', str),
+  styleCommandText: (str) => styleText('cyan', str),
+  styleCommandDescription: (str) => styleText('magenta', str),
   styleItemDescription: (str) => styleText('italic', str),
-  styleOptionTerm: (str) => styleText('green', str),
-  styleSubcommandTerm: (str) => styleText('yellow', str),
+  styleOptionText: (str) => styleText('green', str),
+  styleArgumentText: (str) => styleText('yellow', str),
+  styleSubcommandText: (str) => styleText('blue', str),
 });
 
 program.description('d '.repeat(100));
-program
-  .option('-s', 'short flag')
-  .option('-f, --flag', 'short and long flag')
-  .option('--long <number>', 'l '.repeat(100));
-
-program
-  .command('sub1', 'sssss '.repeat(33))
-  .command('sub2', 'subcommand 2 description')
-  .command('sub3', 'subcommand 3 description');
+program.option('-s', 'short flag').option('--long <number>', 'l '.repeat(100));
 
 program.addHelpText(
-  'afterAll',
+  'after',
   styleText('italic', '\nThis is additional help text.'),
 );
+
+// ToDo: use action subcommands, as more common.
+// ToDo: do one subcommand with multiple arguments and include in the Try section.
+
+program.command('esses').description('sssss '.repeat(33));
+
+program
+  .command('print')
+  .description('print files')
+  .argument('<files...>', 'files to queue for printing');
 
 program.parse();
 
 // Try the following:
-//    node color-help.mjs --help
+//    node color-help-style.mjs --help
 //    NO_COLOR=1 node color-help.mjs --help
+//    node color-help-style.mjs copy --help
