7c83ea8a9a0b7e16b1d8f7b7585c5a7b32e7be71
John Gee <john@ruru.gen.nz>
2025-02-24 22:39:58 +1300

    Add example of using help groups

--- examples/help-groups.js
+++ examples/help-groups.js
@@ -1,47 +1,75 @@
 const { Command, Option } = require('commander');
 
-// The help group can be set explicitly on an Option or Command using `.helpGroup()`,
-// or by setting the default group before adding the option or command.
+// Show the two approaches for adding help groups, and how to customise the built-in help and version.
 
 const program = new Command();
+const devOptionsTitle = 'Development Options:';
+const managementCommandsTitle = 'Management Commands:';
 
-const directGroups = program
-  .command('direct-groups')
-  .description('example with help groups set by using .helpGroup()')
+// The high-level approach is use .optionsGroup() and .commandsGroup() before adding the options/commands.
+const docker1 = program
+  .command('docker1')
+  .description('help groups created using .optionsGroup() and .commandsGroup()')
+  .addOption(new Option('-h, --hostname <name>', 'container host name'))
+  .addOption(new Option('-p, --port <number>', 'container port number'))
+  .optionsGroup(devOptionsTitle)
+  .option('-d, --debug', 'add extra trace information')
+  .option('-w, --watch', 'run and relaunch service on file changes');
+
+docker1
+  .command('run')
+  .description('create and run a new container from an image');
+docker1.command('exec').description('execute a command in a running container');
+
+docker1.commandsGroup(managementCommandsTitle);
+docker1.command('images').description('manage images');
+docker1.command('volumes').description('manage volumes');
+
+// The low-level approach is using .helpGroup() on the Option or Command.
+const docker2 = program
+  .command('docker2')
+  .description('help groups created using .helpGroup()')
+  .addOption(new Option('-h, --hostname <name>', 'container host name'))
+  .addOption(new Option('-p, --port <number>', 'container port number'))
   .addOption(
     new Option('-d, --debug', 'add extra trace information').helpGroup(
-      'Development Options:',
+      devOptionsTitle,
     ),
-  );
-directGroups
-  .command('watch')
-  .description('run project in watch mode')
-  .helpGroup('Development Commands:');
-
-const defaultGroups = program
-  .command('default-groups')
-  .description(
-    'example with help groups set by using optionsGroup/commandsGroup ',
   )
-  .optionsGroup('Development Options:')
-  .option('-d, --debug', 'add extra trace information', 'Development Options:');
-defaultGroups.commandsGroup('Development Commands:');
-defaultGroups.command('watch').description('run project in watch mode');
+  .addOption(
+    new Option(
+      '-w, --watch',
+      'run and relaunch service on file changes',
+    ).helpGroup(devOptionsTitle),
+  );
 
+docker2
+  .command('run')
+  .description('create and run a new container from an image');
+docker2.command('exec').description('execute a command in a running container');
+
+docker2
+  .command('images')
+  .description('manage images')
+  .helpGroup(managementCommandsTitle);
+docker2
+  .command('volumes')
+  .description('manage volumes')
+  .helpGroup(managementCommandsTitle);
+
+// Customise group for built-ins by explicitly adding them with default group set.
 program
   .command('built-in')
-  .description(
-    'changing the help for built-ins by explicitly customising built-in',
-  )
+  .description('help groups for help and version')
   .optionsGroup('Built-in Options:')
   .version('v2.3.4')
-  .helpOption('-h, --help') // get default group by customising help option
+  .helpOption('-h, --help')
   .commandsGroup('Built-in Commands:')
-  .helpCommand('help'); // get default group by customising help option
+  .helpCommand('help');
 
 program.parse();
 
 // Try the following:
-//    node help-groups.js help direct-groups
-//    node help-groups.js help default-groups
+//    node help-groups.js help docker1
+//    node help-groups.js help docker2
 //    node help-groups.js help built-in
