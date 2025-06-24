5c2e4347a985f9d34bf8e03cbefa3c6e020d6910
Yury Semikhatsky <yurys@chromium.org>
2025-06-23 18:10:27 -0700

    Allow optional choice option without a value before optional arguments

--- tests/options.choices.test.js
+++ tests/options.choices.test.js
@@ -59,3 +59,51 @@ describe('choices parameter is treated as readonly, per TypeScript declaration',
     }).toThrow();
   });
 });
+
+test('optional choice followed by an optional argument', () => {
+  const program = new commander.Command();
+  const command = program
+    .command('test [test-filter]')
+    .addOption(
+      new commander.Option('--update-snapshots [value]').choices(['all', 'missing', 'none']),
+    );
+  program.parse(['test', '--update-snapshots', 'tests/test.e2e.ts:23'], { from: 'user' });
+  expect(command.opts().updateSnapshots).toBe(true);
+  expect(command.args).toEqual(['tests/test.e2e.ts:23']);
+});
+
+test('optional choice without a value and unsupported argument is an error', () => {
+  const program = new commander.Command();
+  program
+    .exitOverride()
+    .command('test')
+    .addOption(
+      new commander.Option('--update-snapshots [value]').choices(['all', 'missing', 'none']),
+    );
+  expect(() => {
+    program.parse(['test', '--update-snapshots', 'foo'], { from: 'user' });
+  }).toThrow(`error: too many arguments for 'test'. Expected 0 arguments but got 1.`);
+});
+
+test('optional choice with preset followed by an optional variadic argument', () => {
+  const program = new commander.Command();
+  const command = program
+    .command('test [test-filter...]')
+    .addOption(
+      new commander.Option('-u, --update-snapshots [value]').choices(['all', 'missing', 'none']).preset('missing'),
+    );
+  program.parse(['test', '-u', 'foo.spec.ts', 'bar.spec.ts'], { from: 'user' });
+  expect(command.opts().updateSnapshots).toBe('missing');
+  expect(command.args).toEqual(['foo.spec.ts', 'bar.spec.ts']);
+});
+
+test('optional choice as last option', () => {
+  const program = new commander.Command();
+  const command = program
+    .command('test [test-filter...]')
+    .addOption(
+      new commander.Option('-u, --update-snapshots [value]').choices(['all', 'missing', 'none']).preset('missing'),
+    );
+  program.parse(['test', '--update-snapshots'], { from: 'user' });
+  expect(command.opts().updateSnapshots).toBe('missing');
+});
