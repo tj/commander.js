1aff919c25c1f6618f6507c150f58be9bf1dd4e8
jefrypozo <jefrypozo@gmail.com>
2025-06-30 23:19:28 -0400

    Add MissingArgumentError with argumentName property

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Argument } = require('./lib/argument.js');
 const { Command } = require('./lib/command.js');
-const { CommanderError, InvalidArgumentError } = require('./lib/error.js');
+const {
+  CommanderError,
+  InvalidArgumentError,
+  MissingArgumentError,
+} = require('./lib/error.js');
 const { Help } = require('./lib/help.js');
 const { Option } = require('./lib/option.js');
 
@@ -22,3 +26,4 @@ exports.Help = Help;
 exports.CommanderError = CommanderError;
 exports.InvalidArgumentError = InvalidArgumentError;
 exports.InvalidOptionArgumentError = InvalidArgumentError; // Deprecated
+exports.MissingArgumentError = MissingArgumentError;
