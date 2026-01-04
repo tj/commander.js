1a60fd49866a8f9b7ffdfc801696e5d18826c2be
John Gee <john@ruru.gen.nz>
2026-01-04 14:09:48 +1300

    Restore loop for hasActionHandler in allowExcessArguments test

--- tests/command.allowExcessArguments.test.js
+++ tests/command.allowExcessArguments.test.js
@@ -5,109 +5,114 @@ const assert = require('node:assert/strict');
 // Not testing output, just testing whether an error is detected.
 
 describe('allowExcessArguments', () => {
-  function configureCommand(cmd) {
-    cmd.exitOverride().configureOutput({
-      writeErr: () => {}, // suppress error output
-    });
-    cmd.action(() => {}); // actions normally have action handlers
-  }
+  const cases = [true, false];
+  cases.forEach((hasActionHandler) => {
+    describe(`when ${hasActionHandler ? 'with' : 'without'} action handler: ${hasActionHandler}`, () => {
+      function configureCommand(cmd) {
+        cmd.exitOverride().configureOutput({
+          writeErr: () => {}, // suppress error output
+        });
+        if (hasActionHandler) cmd.action(() => {});
+      }
 
-  test('when specify excess program argument then error by default', () => {
-    const program = new commander.Command();
-    configureCommand(program);
+      test('when specify excess program argument then error by default', () => {
+        const program = new commander.Command();
+        configureCommand(program);
 
-    assert.throws(() => {
-      program.parse(['excess'], { from: 'user' });
-    });
-  });
+        assert.throws(() => {
+          program.parse(['excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess program argument and allowExcessArguments(false) then error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.allowExcessArguments(false);
+      test('when specify excess program argument and allowExcessArguments(false) then error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.allowExcessArguments(false);
 
-    assert.throws(() => {
-      program.parse(['excess'], { from: 'user' });
-    });
-  });
+        assert.throws(() => {
+          program.parse(['excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess program argument and allowExcessArguments() then no error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.allowExcessArguments();
+      test('when specify excess program argument and allowExcessArguments() then no error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.allowExcessArguments();
 
-    assert.doesNotThrow(() => {
-      program.parse(['excess'], { from: 'user' });
-    });
-  });
+        assert.doesNotThrow(() => {
+          program.parse(['excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess program argument and allowExcessArguments(true) then no error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.allowExcessArguments(true);
+      test('when specify excess program argument and allowExcessArguments(true) then no error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.allowExcessArguments(true);
 
-    assert.doesNotThrow(() => {
-      program.parse(['excess'], { from: 'user' });
-    });
-  });
+        assert.doesNotThrow(() => {
+          program.parse(['excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess command argument then error (by default)', () => {
-    const program = new commander.Command();
-    const sub = program.command('sub');
-    configureCommand(sub);
+      test('when specify excess command argument then error (by default)', () => {
+        const program = new commander.Command();
+        const sub = program.command('sub');
+        configureCommand(sub);
 
-    assert.throws(() => {
-      program.parse(['sub', 'excess'], { from: 'user' });
-    });
-  });
+        assert.throws(() => {
+          program.parse(['sub', 'excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess command argument and allowExcessArguments(false) then error', () => {
-    const program = new commander.Command();
-    const sub = program.command('sub').allowExcessArguments(false);
-    configureCommand(sub);
+      test('when specify excess command argument and allowExcessArguments(false) then error', () => {
+        const program = new commander.Command();
+        const sub = program.command('sub').allowExcessArguments(false);
+        configureCommand(sub);
 
-    assert.throws(() => {
-      program.parse(['sub', 'excess'], { from: 'user' });
-    });
-  });
+        assert.throws(() => {
+          program.parse(['sub', 'excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify expected arg and allowExcessArguments(false) then no error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.argument('<file>').allowExcessArguments(false);
+      test('when specify expected arg and allowExcessArguments(false) then no error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.argument('<file>').allowExcessArguments(false);
 
-    assert.doesNotThrow(() => {
-      program.parse(['file'], { from: 'user' });
-    });
-  });
+        assert.doesNotThrow(() => {
+          program.parse(['file'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess after <arg> and allowExcessArguments(false) then error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.argument('<file>').allowExcessArguments(false);
+      test('when specify excess after <arg> and allowExcessArguments(false) then error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.argument('<file>').allowExcessArguments(false);
 
-    assert.throws(() => {
-      program.parse(['file', 'excess'], { from: 'user' });
-    });
-  });
+        assert.throws(() => {
+          program.parse(['file', 'excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify excess after [arg] and allowExcessArguments(false) then error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.argument('[file]').allowExcessArguments(false);
+      test('when specify excess after [arg] and allowExcessArguments(false) then error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.argument('[file]').allowExcessArguments(false);
 
-    assert.throws(() => {
-      program.parse(['file', 'excess'], { from: 'user' });
-    });
-  });
+        assert.throws(() => {
+          program.parse(['file', 'excess'], { from: 'user' });
+        });
+      });
 
-  test('when specify args for [args...] and allowExcessArguments(false) then no error', () => {
-    const program = new commander.Command();
-    configureCommand(program);
-    program.argument('[files...]').allowExcessArguments(false);
+      test('when specify args for [args...] and allowExcessArguments(false) then no error', () => {
+        const program = new commander.Command();
+        configureCommand(program);
+        program.argument('[files...]').allowExcessArguments(false);
 
-    assert.doesNotThrow(() => {
-      program.parse(['file1', 'file2', 'file3'], { from: 'user' });
+        assert.doesNotThrow(() => {
+          program.parse(['file1', 'file2', 'file3'], { from: 'user' });
+        });
+      });
     });
   });
 });
