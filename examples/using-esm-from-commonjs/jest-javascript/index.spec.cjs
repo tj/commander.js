01ce5d0cd7e845d6ed749ab57616ec9c173cf91f
John Gee <john@ruru.gen.nz>
2026-05-22 17:14:52 +1200

    Remove jest esm examples (#2517)

--- examples/using-esm-from-commonjs/jest-javascript/index.spec.cjs
+++
@@ -1,9 +0,0 @@
-const { Command } = require('commander');
-const { test, expect } = require('@jest/globals');
-
-test('when set command name then get command name', () => {
-  const program = new Command();
-  const name = 'my-cli';
-  program.name(name);
-  expect(program.name()).toBe(name);
-});
--- examples/using-esm-from-commonjs/jest-javascript/jest.config.js
+++
@@ -1,6 +0,0 @@
-// List the dependencies that are pure esm and need transformation.
-const esmModules = ['commander'].join('|'); // redundant join in case you have others
-
-module.exports = {
-  transformIgnorePatterns: [`node_modules/(?!(${esmModules})/)`],
-};
